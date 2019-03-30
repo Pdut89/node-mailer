0d9b3ae56158565f4624c1ccc413a318ff866a94
Pieter <pieter_du_toit@hotmail.com>
2019-03-30 15:10:59 +0200

    enable cors

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ app.use(bodyParser.json())
 app.use(morgan('tiny'))
 app.use(helmet())
 
+// https://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next()
+})
+
 const transporter = nodeMailer.createTransport({
   host: 'smtp.zoho.com',
   port: 465,
