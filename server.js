787b2db86c54004e2653ddb314dea1ce39f19794
Pieter <pdutoit@fullfacing.com>
2020-08-05 08:45:27 +0200

    add generic mail route

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ const morgan = require('morgan')
 const helmet = require('helmet')
 const cors = require('cors')
 
-const { PORT, USERNAME, PASSWORD, HOST, RECIPIENT } = process.env
+const { 
+  PORT = 5000, 
+  USERNAME, 
+  PASSWORD, 
+  HOST, 
+  RECIPIENT, 
+  SMTP_PORT = 465 
+} = process.env
 
 const app = express()
 
@@ -22,8 +29,8 @@ app.use(helmet())
 
 const transporter = nodeMailer.createTransport({
   host: HOST,
-  port: 465,
-  secure: true,
+  port: SMTP_PORT,
+  secure: SMTP_PORT === "465",
   auth: {
     user: USERNAME,
     pass: PASSWORD
@@ -36,13 +43,32 @@ const email = new Email({
   preview: false
 })
 
+const renderMessage = ({ message, email, phoneNumber }) => `
+  <div>
+    <p>You have recieved a message from ${email} </p>
+    <strong>Message:</strong>
+    <p>${message}</p>
+    <p> <a href = "mailto:${email}">Reply to sender</a> </p>
+    ${phoneNumber.length > 0 ? `
+      <p>Sender phone number: <a href="tel:${phoneNumber}">${phoneNumber}</a> </p>
+    ` : `
+      <p>Sender did not include a phone number.</p>
+    `}
+  </div>
+`
+
 app.get('/', (req, res) => res.status(200).send(
   'Node-mailer. Nothing here. See server.js for email POST request details.'
 ))
 
 app.post('/send', (req, res) => {
   const {
-    name, surname, phoneNumber, emailAddress, subject, message
+    name, 
+    surname, 
+    phoneNumber, 
+    emailAddress, 
+    subject, 
+    message
   } = req.body
 
   const emailOptions = {
@@ -52,7 +78,12 @@ app.post('/send', (req, res) => {
       to: RECIPIENT
     },
     locals: {
-      name, surname, phoneNumber, emailAddress, subject, message
+      name, 
+      surname, 
+      phoneNumber, 
+      emailAddress, 
+      subject, 
+      message
     }
   }
 
@@ -62,14 +93,38 @@ app.post('/send', (req, res) => {
         message: 'Email sent successfully.'
       })
     })
-    .catch(err => {
-      console.log('error: ', err)
+    .catch(error => {
+      console.log('error: ', error)
       res.status(400).send({
         error: err.toString()
       })
     })
 })
 
-app.listen(PORT, function(req, res){
+app.post('/generic', async (req, res) => {
+  const {
+    to,
+    ...rest
+  } = req.body
+
+  try {
+    await transporter.sendMail({
+      from: USERNAME,
+      to,
+      subject: 'New Web Message Received',
+      html: renderMessage(rest),
+    })
+    res.status(200).send({
+      message: 'Email sent successfully.'
+    })
+  } catch (error) {
+    console.log('error: ', error)
+    res.status(400).send({
+      error: err.toString()
+    })
+  }
+})
+
+app.listen(PORT, () => {
   console.log('Server is running on port: ', PORT)
 })
